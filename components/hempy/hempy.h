1b9ebdb11233eaad48c77e9f0434abd56d1e5439
Bitmuki <bitmuki@fuck.off>
2024-10-17 22:50:08 +0200

    Integrate HA helpers - P6

--- components/hempy/hempy.h
+++ components/hempy/hempy.h
@@ -4,7 +4,7 @@
 #include "esphome/core/component.h"
 #include "esphome/core/log.h"
 #include "esphome/components/sensor/sensor.h"
-//#include "esphome/core/application.h"
+#include "esphome/core/application.h"
 
 namespace esphome {
 namespace hempy {
